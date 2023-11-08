410a4fafc5bebb9cb44bdf02c902cbf6e059e706
tedskiii <theodorekasyfillah06@gmail.com>
2023-11-08 14:28:27 +0700

    Push Tutor 7 yay

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/widgets/left_drawer.dart';
-import 'package:shopping_list/shoplist_form.dart';
+import 'package:shopping_list/screens/shoplist_form.dart';
 
  class MyHomePage extends StatelessWidget {
     MyHomePage({Key? key}) : super(key: key);
