cd6f679ca1204ce09420b62244643b014278b34d
tedskiii <theodorekasyfillah06@gmail.com>
2023-11-22 14:40:38 +0700

    Push Tutor 9 lagi dan lagi

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
 import 'package:shopping_list/screens/menu.dart';
+import 'package:shopping_list/screens/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -38,7 +39,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
         useMaterial3: true,
       ),
-      home: MyHomePage(),
+      home: LoginPage(),
             )
     );
   }
